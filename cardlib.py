007d010178ac3685c865a11e9bdadffd1f40f46a
Benjamin  Jonsson <benjaminjonsson@Benjamins-MacBook-Pro.local>
2022-06-20 14:46:07 +0200

    Structure maybe final

--- cardlib.py
+++ cardlib.py
@@ -3,23 +3,51 @@ from enum import Enum
 import random
 
 class PlayingCard(ABC):
-    def __init__(self,value,suit):
-        self.value = value
+    @abstractmethod
+    def __init__(self,suit):
         self.suit = suit
-    
-    def JackCard(Suit):
-        pass
-
-    def QueenCard(Suit):
-        pass
-
-    def KingCard(Suit):
-        pass
-
-    def AceCard(Suit):
-        pass
 
     @abstractmethod
+    def get_value():
+        pass
+
+class NumberedCard(PlayingCard):
+    def __init__(self, suit, value):
+        super().__init__(self,suit)
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    
+class JackCard(PlayingCard):
+    def __init__(self, suit):
+        super().__init__(self,suit)
+
+    def get_value():
+        pass
+
+
+class QueenCard(PlayingCard):
+    def __init__(self, suit):
+        super().__init__(suit)
+
+    def get_value():
+        pass
+
+
+class KingCard(PlayingCard):
+    def __init__(self, suit):
+        super().__init__(suit)
+
+    def get_value():
+        pass
+
+
+class AceCard(PlayingCard):
+    def __init__(self, suit):
+        super().__init__(suit)
+
     def get_value():
         pass
 
@@ -31,19 +59,20 @@ class Suit(Enum):
     Diamonds = 3
 
 
-class Hand(PlayingCard):
+class Hand(list):
     def __init__(self):
         super().__init__(self)
 
-    def add_card():
-        pass
+    def add_card(self,cards = []):
+        for i in cards:
+            self.append(i)
 
     def drop_cards(self,indList):
         for i in range(indList):
             self.pop(i)
 
-    def sort():
-        pass
+    def sort(self):
+        self.sort()
 
     def best_poker_hand(self,cards = []):
         pass
@@ -52,9 +81,9 @@ class Hand(PlayingCard):
         pass
 
 
-class StandardDeck(PlayingCard):
+class StandardDeck(list):
     def __init__(self):
-        pass
+        super().__init__()
 
     def shuffle(self):
         random.shuffle(self)
@@ -62,13 +91,7 @@ class StandardDeck(PlayingCard):
     def draw(self,loc,num):
         pass 
 
-    def get_value():
-        pass
 
-
-class PokerHand(PlayingCard):
+class PokerHand(list):
     def __init__(self):
-        pass
-
-    def get_value():
         pass
